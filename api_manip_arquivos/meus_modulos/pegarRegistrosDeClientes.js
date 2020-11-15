4da895095d8b62a61b7b4ea42af451fe9b27c92b
Deive <deivealtoe@gmail.com>
2020-11-15 16:26:37 -0300

    Add files via upload

--- api_manip_arquivos/meus_modulos/pegarRegistrosDeClientes.js
+++ api_manip_arquivos/meus_modulos/pegarRegistrosDeClientes.js
@@ -15,6 +15,22 @@ async function pegarRegistrosDeClientes() {
 
         registros = JSON.parse(registros);
 
+        registros = registros.map((registro) => {
+            if (registro.estado_civil == 1) {
+                registro.estado_civil = 'Solteiro';
+            } else if (registro.estado_civil == 2) {
+                registro.estado_civil = 'Casado';
+            } else if (registro.estado_civil == 3) {
+                registro.estado_civil = 'Divorciado';
+            } else if (registro.estado_civil == 4) {
+                registro.estado_civil = 'Viúvo';
+            } else if (registro.estado_civil == 5) {
+                registro.estado_civil = 'Separado';
+            }
+
+            return registro;
+        });
+
         return registros;
     } catch (error) {
         if (error.code == 'ENOENT') { // ENOENT == Arquivo ou diretório não encontrado. Com essa função assíncrona o arquivo é criado
